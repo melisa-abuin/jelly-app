59f22e5eaad5740f40192e39ff76b5413c39bfe4
Stannnnn <stann@live.nl>
2025-04-19 14:15:26 +0200

    added manifest settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,32 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'My Cool App',
+                short_name: 'CoolApp',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#ffffff',
+                theme_color: '#317EFB',
+                icons: [
+                    {
+                        src: 'icons/icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'icons/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
     define: {
         __NPM_LIFECYCLE_EVENT__: JSON.stringify(process.env.npm_lifecycle_event),
     },
