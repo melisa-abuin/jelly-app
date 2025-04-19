786d9ebaac515a592696cbf6d9e45a376bc4df74
Stannnnn <stann@live.nl>
2025-04-19 02:32:22 +0200

    yarn add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
     define: {
         __NPM_LIFECYCLE_EVENT__: JSON.stringify(process.env.npm_lifecycle_event),
     },
