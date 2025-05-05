68c1ec302c710b7f22907e4c6ceb7f5b48131a17
Stannnnn <stann@live.nl>
2025-05-05 19:33:43 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/jelly-app/sw.js', { scope: '/jelly-app/' })})}
