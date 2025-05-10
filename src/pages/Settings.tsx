98c3dd5c44a61645dc8ebd373a9be940943bc1e4
Simonh2o <simon@mkdir.no>
2025-05-10 19:35:50 +0200

    added more settings temp

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,3 +1,4 @@
+import { CheckCircleFillIcon, ChevronDownIcon } from '@primer/octicons-react'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useJellyfinContext } from '../context/JellyfinContext/JellyfinContext'
@@ -66,9 +67,7 @@ const Settings = ({ onLogout }: SettingsProps) => {
     return (
         <div className="settings-page">
             <div className="section appearance">
-                <div className="info">
-                    <div className="title">Appearance</div>
-                </div>
+                <div className="title">Appearance</div>
                 <div className="options noSelect">
                     <div
                         className={`option light ${theme === 'light' ? 'active' : ''}`}
@@ -93,75 +92,244 @@ const Settings = ({ onLogout }: SettingsProps) => {
                     </div>
                 </div>
             </div>
-            <div className="section quality">
-                <div className="info">
-                    <div className="title">Audio Quality</div>
-                    <div className="desc">
-                        <p>
-                            1. Some options to enable transcoding, off by default. 2. The audio will be converted to AAC
-                            320/128kbps, more container/bitrate options, or rigid/tight selection? 3. Source file
-                            (quality) enabled by default, some info about that first or after mentioning transcoding?
-                            Offline support, PWA possibilities, download tracks, albums, playlists, favorites?
-                        </p>
+            <div className="section playback">
+                <div className="title">Playback</div>
+                <div className="inner">
+                    <div className="container">
+                        <div className="info">
+                            <div className="subtitle">Streaming Quality</div>
+                            <div className="subdesc">
+                                Adjusting audio quality enables server-side transcoding, converting to a compatible
+                                format with a lower bitrate for potentially smoother playback and reduced bandwidth
+                            </div>
+                        </div>
+                        <div className="options noSelect">
+                            <div className="option source active">
+                                <div className="status">
+                                    <CheckCircleFillIcon size={16} />
+                                </div>
+                                <div className="details">
+                                    <div className="title">Source</div>
+                                    <div className="desc">
+                                        Direct playback of the original audio source without modifications
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="option high">
+                                <div className="status">
+                                    <CheckCircleFillIcon size={16} />
+                                </div>
+                                <div className="details">
+                                    <div className="title">
+                                        High <span className="bitrate">320 kbps</span>
+                                    </div>
+                                    <div className="desc">
+                                        Superior sound quality, perfect for immersive listening with moderate data usage
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="option medium">
+                                <div className="status">
+                                    <CheckCircleFillIcon size={16} />
+                                </div>
+                                <div className="details">
+                                    <div className="title">
+                                        Medium <span className="bitrate">256 kbps</span>
+                                    </div>
+                                    <div className="desc">
+                                        Crisp audio with a balanced blend of quality and data efficiency
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="option low">
+                                <div className="status">
+                                    <CheckCircleFillIcon size={16} />
+                                </div>
+                                <div className="details">
+                                    <div className="title">
+                                        Low <span className="bitrate">192 kbps</span>
+                                    </div>
+                                    <div className="desc">
+                                        Solid quality tailored for streaming with reduced bandwidth
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="option minimal">
+                                <div className="status">
+                                    <CheckCircleFillIcon size={16} />
+                                </div>
+                                <div className="details">
+                                    <div className="title">
+                                        Minimal <span className="bitrate">128 kbps</span>
+                                    </div>
+                                    <div className="desc">
+                                        Essential audio quality optimized for the lowest data consumption
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="inner">
+                    <div className="container">
+                        <div className="info">
+                            <div className="subtitle">Download Quality</div>
+                            <div className="subdesc">
+                                Adjusting download quality converts audio to a compatible format with a lower bitrate,
+                                balancing sound fidelity and storage space, used in offline mode
+                            </div>
+                        </div>
+                        <div className="options noSelect">
+                            <div className="option source active">
+                                <div className="status">
+                                    <CheckCircleFillIcon size={16} />
+                                </div>
+                                <div className="details">
+                                    <div className="title">Source</div>
+                                    <div className="desc">Downloads the original audio file without modifications</div>
+                                </div>
+                            </div>
+                            <div className="option high">
+                                <div className="status">
+                                    <CheckCircleFillIcon size={16} />
+                                </div>
+                                <div className="details">
+                                    <div className="title">
+                                        High <span className="bitrate">320 kbps</span>
+                                    </div>
+                                    <div className="desc">
+                                        Superior sound quality, ideal for immersive listening with moderate storage
+                                        needs
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="option medium">
+                                <div className="status">
+                                    <CheckCircleFillIcon size={16} />
+                                </div>
+                                <div className="details">
+                                    <div className="title">
+                                        Medium <span className="bitrate">256 kbps</span>
+                                    </div>
+                                    <div className="desc">
+                                        Crisp audio with a balance of quality and storage efficiency
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="option low">
+                                <div className="status">
+                                    <CheckCircleFillIcon size={16} />
+                                </div>
+                                <div className="details">
+                                    <div className="title">
+                                        Low <span className="bitrate">192 kbps</span>
+                                    </div>
+                                    <div className="desc">Solid quality optimized for reduced storage use</div>
+                                </div>
+                            </div>
+                            <div className="option minimal">
+                                <div className="status">
+                                    <CheckCircleFillIcon size={16} />
+                                </div>
+                                <div className="details">
+                                    <div className="title">
+                                        Minimal <span className="bitrate">128 kbps</span>
+                                    </div>
+                                    <div className="desc">Essential audio quality for minimal storage consumption</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="inner row">
+                    <div className="container">
+                        <div className="info">
+                            <div className="subtitle">Offline Mode</div>
+                            <div className="subdesc">
+                                When you go offline, only the media you have downloaded will be available
+                            </div>
+                        </div>
+                        <label className="switch">
+                            <input type="checkbox"></input>
+                            <span className="slider"></span>
+                        </label>
+                    </div>
+                </div>
+                <div className="inner row">
+                    <div className="container">
+                        <div className="info">
+                            <div className="subtitle">Volume Normalization</div>
+                            <div className="subdesc">
+                                Plays tracks or albums at a consistent volume, requires audio files or library settings
+                                to be properly configured on the server-side in order for loudness normalization to work
+                            </div>
+                        </div>
+
+                        <div className="filter">
+                            <select defaultValue="Off">
+                                <option value="Off">Off</option>
+                                <option value="Tracks">Tracks</option>
+                                <option value="Albums">Albums</option>
+                            </select>
+                            <div className="icon">
+                                <ChevronDownIcon size={12} />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="section about">
-                <div className="info">
-                    <div className="title">About</div>
-                    <div className="desc">
-                        <p className="subtitle">Jelly Music App - Version 0.1</p>
-                        <p>An open source music player for Jellyfin</p>
-                        <p>
-                            Carefully crafted with great attention to detail, aiming to reduce noise and distractions
-                            with a minimalistic & lightweight interface:
-                            <span className="mantra"> "the quieter you become, the more you are able to hear"</span>
-                        </p>
-                        <p className="subfooter">
-                            <span>Source code is freely available on </span>
-                            <a
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="textlink"
-                                href="https://github.com/Stannnnn/jelly-app"
-                            >
-                                GitHub
-                            </a>
-                            <span> and is licensed under the MIT license</span>
-                        </p>
-                    </div>
+                <div className="title">About</div>
+                <div className="desc">
+                    <p className="subtitle">Jelly Music App - Version 0.1</p>
+                    <p>An open source music player for Jellyfin</p>
+                    <p>
+                        Carefully crafted with great attention to detail, aiming to reduce noise and distractions with a
+                        minimalistic & lightweight interface:
+                        <span className="mantra"> "the quieter you become, the more you are able to hear"</span>
+                    </p>
+                    <p className="subfooter">
+                        <span>Source code is freely available on </span>
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="textlink"
+                            href="https://github.com/Stannnnn/jelly-app"
+                        >
+                            GitHub
+                        </a>
+                        <span> and is licensed under the MIT license</span>
+                    </p>
                 </div>
             </div>
             <div className="section session">
-                <div className="info">
-                    <div className="title">Session</div>
-                    <div className="desc">
-                        <p>
-                            Currently connected to{' '}
-                            <a target="_blank" rel="noopener noreferrer" className="textlink" href={api.auth.serverUrl}>
-                                {api.auth.serverUrl}
-                            </a>{' '}
-                            {latency !== null && (
-                                <span>
-                                    <span>with {latency}ms latency</span>
-                                </span>
-                            )}
-                        </p>
-                        <p>
-                            Last login: {lastLogin} {clientIp ? ` from ${clientIp}` : ''}
-                        </p>
-                        <p>
-                            Played{' '}
-                            {sessionPlayCount !== null && (
-                                <span>
-                                    {sessionPlayCount} {sessionPlayCount === 1 ? 'track' : 'tracks'}
-                                </span>
-                            )}{' '}
-                            since login
-                        </p>
-                    </div>
+                <div className="title">Session</div>
+                <div className="desc">
+                    <p>
+                        Currently connected to{' '}
+                        <a target="_blank" rel="noopener noreferrer" className="textlink" href={api.auth.serverUrl}>
+                            {api.auth.serverUrl}
+                        </a>{' '}
+                        {latency !== null && (
+                            <span>
+                                <span>with {latency}ms latency</span>
+                            </span>
+                        )}
+                    </p>
+                    <p>
+                        Last login: {lastLogin} {clientIp ? ` from ${clientIp}` : ''}
+                    </p>
+                    <p>
+                        Played{' '}
+                        {sessionPlayCount !== null && (
+                            <span>
+                                {sessionPlayCount} {sessionPlayCount === 1 ? 'track' : 'tracks'}
+                            </span>
+                        )}{' '}
+                        since login
+                    </p>
                 </div>
-                <div className="options noSelect">
+                <div className="actions noSelect">
                     <button onClick={handleLogout} className="logout-button">
                         Logout
                     </button>
