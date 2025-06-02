eac4fb0e1c7fac8864d312fbbaab896be37883ea
melisa-abuin <melisa.abuin@gmail.com>
2025-06-02 11:27:44 +0200

    feat(context): read param from url

--- src/context/FilterContext/FilterContextProvider.tsx
+++ src/context/FilterContext/FilterContextProvider.tsx
@@ -1,17 +1,49 @@
 import { BaseItemKind, ItemSortBy, SortOrder } from '@jellyfin/sdk/lib/generated-client'
 import { ReactNode, useMemo, useState } from 'react'
 import { FilterContext } from './FilterContext'
+import { useLocation, useSearchParams } from 'react-router-dom'
 
 export type IFilterContext = ReturnType<typeof useInitialState>
 
+enum SortInitialState {
+    Added = 'Added',
+    Tracks = 'Tracks',
+    Released = 'Released',
+    Runtime = 'Runtime',
+    Random = 'Random',
+    Artists = 'Artists',
+    Albums = 'Albums',
+    None = ''
+}
+
+const pathToSortMap: Record<string, SortInitialState> = {
+    '/tracks': SortInitialState.Added,
+    '/albums': SortInitialState.Added,
+    '/genre': SortInitialState.Added,
+    '/favorites': SortInitialState.Tracks
+}
+
+const isValidSortValue = (val: string): val is SortInitialState => {
+    return Object.values(SortInitialState)
+        .map(v => v.toLowerCase())
+        .includes(val.toLowerCase())
+}
+
+
 const useInitialState = () => {
-    const [sort, setSort] = useState(
-        location.pathname === '/tracks' || location.pathname === '/albums' || location.pathname === '/genre'
-            ? 'Added'
-            : location.pathname === '/favorites'
-            ? 'Tracks'
-            : ''
-    )
+    const location = useLocation()
+    const [searchParams] = useSearchParams()
+    const queryFilter = searchParams.get('filter')
+
+    const filterFromQuery = queryFilter && isValidSortValue(queryFilter)
+    ? (Object.values(SortInitialState).find(
+        v => v.toLowerCase() === queryFilter.toLowerCase()
+      ) as SortInitialState)
+    : null
+
+    const pathFallback = pathToSortMap[location.pathname] ?? SortInitialState.None
+
+    const [sort, setSort] = useState<SortInitialState>(filterFromQuery || pathFallback)
 
     const jellySort = useMemo(() => {
         let newSortBy: ItemSortBy[]
