b357a8df8ae50a2e6fb1fe63c6637c8daf1f5cc9
Stannnnn <stann@live.nl>
2025-06-24 17:35:44 +0200

    improved error handling in getAllTracks

--- src/utils/getAllTracks.ts
+++ src/utils/getAllTracks.ts
@@ -1,7 +1,11 @@
 import { InfiniteData } from '@tanstack/react-query'
 import { MediaItem } from '../api/jellyfin'
+import { isMediaItem } from '../hooks/usePatchQueries'
 
-export const getAllTracks = (data: InfiniteData<MediaItem[], unknown> | undefined, allowDuplicates = false) => {
+export const getAllTracks = (
+    data: InfiniteData<MediaItem[] | undefined, unknown> | undefined,
+    allowDuplicates = false
+) => {
     if (!data) {
         return []
     }
@@ -9,7 +13,19 @@ export const getAllTracks = (data: InfiniteData<MediaItem[], unknown> | undefine
     const flattened = data.pages
         .map((page, pageIndex) => {
             if (!page) {
-                console.warn('getAllTracks: Page is undefined or null', data)
+                console.error('getAllTracks: Page is undefined or null', data)
+                console.trace()
+                return []
+            }
+
+            if (!Array.isArray(page)) {
+                console.error('getAllTracks: Page is not an array', data)
+                console.trace()
+                return []
+            }
+
+            if (page[0] && !isMediaItem(page[0])) {
+                console.error('getAllTracks: Page does not contain MediaItem objects', data)
                 console.trace()
                 return []
             }
